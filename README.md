bf8a49e0e5cfa1588f8cfdb926f51b588b92a99d
TW61-code <drew69676@gmail.com>
2024-06-19 15:44:12 +1200

    Setup the application files

---
+++ README.md
@@ -0,0 +1,3 @@
+## Git and GitHub Tutorial
+Hi everyone, 
+I am learning some useful command line tools.
---
+++ script.js
@@ -0,0 +1,7 @@
+const app = document.querySelector(".root")
+
+app.innerHTML = `<h1>Hello World</h1>`
+app.style.color = "#ffff"
+app.style.backgroundColor = 'red'
+
+document.body.style.backgroundColor = "blue"
